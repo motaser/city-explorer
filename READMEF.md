abaec65ab12aa2915c343b6fe06cd3167e4abd1d
motaser <araidahmontaser@gmail.com>
2021-06-29 09:14:13 +0300

    lab

---
+++ READMEF.md
@@ -0,0 +1,9 @@
+Name of feature: ____lab06____________________________
+
+Estimate of time needed to complete: __4 hours___
+
+Start time: __3pm___
+
+Finish time: __9pm___
+
+Actual time needed to complete:  6 hours
--- lab06/src/App.js
+++ lab06/src/App.js
@@ -1,43 +1,18 @@
-  
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import City from './City';
 
-export class App extends Component {
 
-  constructor(props){
-    super(props);
-    this.state={
-      displayName:'',
-      longitude:'',
-      latitude:''
-    }
-  }
-
-  nameChangeHandler=(e)=>{
-    this.setState({
-      displayName:e.target.value
-      })
-  }
-  submitData=async (e)=>{
-    e.preventDefault()
-    let axiosResponse= await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.e7dafb4666e0c853da004c39e1c164fc&city=${this.state.displayName}&format=json`)
-    this.setState({
-      displayName:axiosResponse.data[0].display_name,
-      longitude:axiosResponse.data[0].lon,
-      latitude:axiosResponse.data[0].lat
-    })
-    console.log(axiosResponse.data[0].lat)
-  }
+ class App extends Component {
   render() {
     return (
-      <div>
-        <form onSubmit={this.submitData}>
-          <input type='text' placeholder='city name....' onChange={(e)=>{this.nameChangeHandler(e)}} />
-          <button>Search</button>
-        </form>
-        <h1>{this.state.displayName}</h1>
-        <h1>{this.state.longitude}</h1>
-        <h1>{this.state.latitude}</h1>
+      <div className="App">
+      
+       <header >
+          <h1>City Name </h1>
+        </header>
+       
+       <City />
       </div>
     )
   }
